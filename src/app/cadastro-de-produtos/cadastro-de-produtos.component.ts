fcf0b8703a38ef750b051e260bf99e623bbbb145
renato <renatopiresfilho.rvpf@gmail.com>
2025-02-17 12:15:39 -0300

    modified:   src/app/cadastro-de-produtos/cadastro-de-produtos.component.html 	modified:   src/app/cadastro-de-produtos/cadastro-de-produtos.component.ts

--- src/app/cadastro-de-produtos/cadastro-de-produtos.component.ts
+++ src/app/cadastro-de-produtos/cadastro-de-produtos.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Produto } from '../modelo/Produto';
-
+import { Produto ,} from '../modelo/Produto';
+import { CommonModule , } from '@angular/common';
 @Component({
   selector: 'app-cadastro-de-produtos',
-  imports: [FormsModule, ReactiveFormsModule, ],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule,],
   templateUrl: './cadastro-de-produtos.component.html',
   styleUrl: './cadastro-de-produtos.component.css'
 })
+
 export class CadastroDeProdutosComponent {
 
+  visBtn : boolean = true;
+  indice : number = -1;
   cadastro = new FormGroup({
-    nomeDoProduto : new FormControl('', Validators.required),
-    dataDeValidade : new FormControl(null, Validators.required),
-    valorDoProduto : new FormControl(null, Validators.required),
-    quantidadeDeProdutos : new FormControl(null, Validators.required),
-    codigoDeBarraDoProduto : new FormControl('', Validators.required)
+    nomeProduto : new FormControl('', Validators.required),
+    validadeProduto : new FormControl(null, Validators.required),
+    valorProduto : new FormControl(null, Validators.required),
+    quantidadeProduto : new FormControl(null, Validators.required),
+    codigoBarraProduto: new FormControl('', Validators.required)
   })
   vetor:Produto[] = []
+
+
   cadastra(){
     this.vetor.push(this.cadastro.value as Produto)
 
@@ -25,6 +30,42 @@ export class CadastroDeProdutosComponent {
 
     console.table(this.vetor)
   }
+  selecionar(indice:number){
+    this.indice = indice;
+
+    this.cadastro.setValue({
+      nomeProduto : this.vetor[indice].nomeProduto,
+      validadeProduto : this.vetor[indice].validadeProduto,
+      valorProduto : this.vetor[indice].valorProduto,
+      quantidadeProduto : this.vetor[indice].quantidadeProduto,
+      codigoBarraProduto : this.vetor[indice].codigoBarraProduto
+    })
+    this.visBtn = false;
+
+  }
+  alterar(){
+    this.vetor[this.indice] = this.cadastro.value as Produto;
+
+    this.cadastro.reset();
+
+    this.visBtn = true;
+    
+  }
+  excluir(){
+    this.vetor.splice(this.indice, 1);
+
+    this.cadastro.reset();
+
+    this.visBtn = true
+
+  }
+  cancelar(){
+    this.cadastro.reset();
+
+    this.visBtn = true
+    
+  }
+
 
 }
 
